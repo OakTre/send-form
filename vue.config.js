80cb3bfa23c85e782adcb3d24269ecb704b7472b
NailGalimov <nailgalimov8@gmail.com>
2023-02-22 00:43:13 +0300

    upd

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,10 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === "production" ? "/send-form/dist/" : "/",
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Authorization',
+    },
+  }
 })
